1417e799c300a6fc6276c920ea09aa523a1da4b4
otaviobrignoni <otavio.678.b@gmail.com>
2026-04-17 15:22:30 -0300

    Implementado o layout do menu principal do Clube da Leitura

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace ClubeDaLeitura.ConsoleApp;
+﻿using System.Text;
+using ClubeDaLeitura.ConsoleApp.Shared;
+
+namespace ClubeDaLeitura.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = Encoding.UTF8;
+        string title = "Clube da Leitura";
+        string[] options = ["Caixas", "Revistas", "Amigos", "Empréstimos", "Sair"];
+        while (true)
+            switch (Utils.Menu(title, options))
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    return;
+            }
     }
 }
