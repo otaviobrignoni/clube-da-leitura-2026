e1e475e34824cb204a4b648d6fa765cf1f70bd38
otaviobrignoni <otavio.678.b@gmail.com>
2026-04-18 17:50:45 -0300

    Alterado tipo do parâmetro em GetAvailable

--- ClubeDaLeitura.ConsoleApp/Shared/Base/BaseUI.cs
+++ ClubeDaLeitura.ConsoleApp/Shared/Base/BaseUI.cs
@@ -15,7 +15,7 @@ public abstract class BaseUI<T> where T : BaseEntity<T>
     public abstract void Remove();
     public abstract void View();
     public abstract T Select(string? title = null, List<T>? entities = null);
-    protected List<T> GetAvailable(List<T>? entities = null)
+    protected List<T> GetAvailable(IEnumerable<T>? entities = null)
     {
         entities ??= [];
         return Repository.GetAll().Where(e => !entities.Contains(e)).ToList();
