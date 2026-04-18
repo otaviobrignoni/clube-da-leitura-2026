c85cc4ebdefec75e1301cfdc034eb0bda5c0b47f
otaviobrignoni <otavio.678.b@gmail.com>
2026-04-18 13:58:00 -0300

    Atualizado cores na classe Colours

--- ClubeDaLeitura.ConsoleApp/Shared/Colours.cs
+++ ClubeDaLeitura.ConsoleApp/Shared/Colours.cs
@@ -3,8 +3,8 @@ namespace ClubeDaLeitura.ConsoleApp.Shared;
 public static class Colours
 {
     public const string Info = "#6e9fcc";
-    public const string Success = "#57e34f";
-    public const string Warning = "#f5bd45";
-    public const string Error = "#de4949";
+    public const string Success = "#6bdf64";
+    public const string Warning = "#fcd965";
+    public const string Error = "#e25858";
     public const string Title = "#78eeea";
 }
